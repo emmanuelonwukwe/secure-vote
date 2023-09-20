d13c41347d9243f939f5fa5795bb440bd1b99f40
emmanuelonwukwe <emmanuelonwukwe202@gmail.com>
2023-09-20 02:52:22 +0100

    Add spinner

--- client/src/layouts/MainLayout.jsx
+++ client/src/layouts/MainLayout.jsx
@@ -1,12 +1,23 @@
 import { Outlet } from "react-router-dom";
 import MainNavbar from "../components/navbar/MainNavbar";
 import MainFooter from "../components/footer/MainFooter";
-import React from "react";
+import React, { useState } from "react";
+import Spinner from "../components/Spinner";
+import { useNavigation } from "react-router-dom";
 
 export default function MainLayout() {
+    const navigation = useNavigation();
+    const [isSpinning, setIsSpinning] = useState(true);
+
+    setTimeout(() => {
+        setIsSpinning(false);
+    }, 2000);
+
     return (
         <React.Fragment>
             <MainNavbar />
+            {isSpinning && <Spinner />}
+            {navigation.state === "loading" && <Spinner />}
             <div className="outlet-area">
                 <Outlet />
             </div>
