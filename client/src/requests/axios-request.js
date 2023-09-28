939ab4c0c276cd4d8b0dbd91eef2ba6d8f33f07a
emmanuelonwukwe <emmanuelonwukwe202@gmail.com>
2023-09-28 13:32:35 +0100

    Update the axios baseUrl for development and production environments

--- client/src/requests/axios-request.js
+++ client/src/requests/axios-request.js
@@ -1,7 +1,12 @@
 import axios from "axios";
+const app_environment = "production";
+const baseURL =
+  app_environment == "development"
+    ? "http://localhost:3000/api/v1"
+    : "https://secure-vote.onrender.com/api/v1";
 
 const axiosRequest = axios.create({
-  baseURL: "http://localhost:3000/api/v1",
+  baseURL: baseURL,
   timeout: 60000,
   headers: { "static-header": "Pass Static data" },
 });
