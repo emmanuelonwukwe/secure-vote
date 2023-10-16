384e8e3693f2e0d1ad60f5c2f759b66125ea0c91
emmanuelonwukwe <emmanuelonwukwe202@gmail.com>
2023-10-16 19:15:26 +0100

    Update the server db config

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ import postgres from 'postgres';
 import env from "dotenv";
 env.config();
 
-const DATABASE_URL = process.env.NODE_ENV == "development" ? process.env.DATABASE_URL_DEV : process.env.PROD;
+const DATABASE_URL = process.env.NODE_ENV == "development" ? process.env.DATABASE_URL_DEV : process.env.DATABASE_URL_PROD;
 const sql =  process.env.NODE_ENV == "development" ? postgres(DATABASE_URL) : postgres(DATABASE_URL, { ssl: "require" }); // will use psql environment variables
 
 export default sql;
