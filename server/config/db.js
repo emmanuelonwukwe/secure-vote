8e4f83f7fcba47d57e149f0b53fcff06fc2a7310
emmanuelonwukwe <emmanuelonwukwe202@gmail.com>
2023-10-17 11:21:39 +0100

    Update the backend db config

--- server/config/db.js
+++ server/config/db.js
@@ -3,6 +3,6 @@ import env from "dotenv";
 env.config();
 
 const DATABASE_URL = process.env.NODE_ENV == "development" ? process.env.DATABASE_URL_DEV : process.env.DATABASE_URL_PROD;
-const sql =  postgres(DATABASE_URL, { ssl: "allow" }); // will use psql environment variables
-console.log(DATABASE_URL);
+const sql =  process.env.NODE_ENV == "development" ? postgres(DATABASE_URL) : postgres(DATABASE_URL, { ssl: "allow" }); // will use psql environment variables
+console.log("Node app connected to DB:", DATABASE_URL);
 export default sql;
